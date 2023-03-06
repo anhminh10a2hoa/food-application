4518ec952e97784a4bd385c7b353377fe879bcc8
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-03-06 22:27:41 +0200

    feat: 🎸 added create table controller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -49,6 +50,41 @@ func GetTable() gin.HandlerFunc {
 
 func CreateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var table models.Table
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			defer cancel()
+			return
+		}
+
+		validationErr := validate.Struct(table)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			defer cancel()
+			return
+		}
+
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+
+		result, insertErr := tableCollection.InsertOne(ctx, table)
+
+		if insertErr != nil {
+			msg := "Table item was not created"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			defer cancel()
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 
 	}
 }
