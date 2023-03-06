80561a2ab49cfc1bf247c33d367087e8ee7bee00
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-03-06 22:26:58 +0200

    feat: 🎸 added get tables controller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,9 +1,14 @@
 package controller
 
 import (
+	"context"
 	"golang-food-application/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,7 +16,18 @@ var tableCollection *mongo.Collection = database.OpenCollection(database.Client,
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing table items"})
+		}
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allTables)
 	}
 }
 
